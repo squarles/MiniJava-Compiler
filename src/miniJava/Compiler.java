2178146cc33046feb4c4c1e18c73ee1354131a32
squarles <quarlessam@gmail.com>
2024-02-20 14:46:13 -0500

    Hardcode removed

--- src/miniJava/Compiler.java
+++ src/miniJava/Compiler.java
@@ -10,7 +10,7 @@ public class Compiler {
 	// Main function, the file to compile will be an argument.
 	public static void main(String[] args) throws FileNotFoundException {
 		ErrorReporter reporter = new ErrorReporter();
-		String path = "C:\\Users\\quarl\\Downloads\\pa1-tests-partial\\pass123.java"; //args[0];
+		String path = args[0];
 		InputStream in = new FileInputStream(new File(path));
 		Scanner scanner = new Scanner(in, reporter);
 		Parser parser = new Parser(scanner, reporter);
